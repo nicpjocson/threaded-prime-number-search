548a3b279348cf07c5fcf105cbccc704dbdd618a
Nicole Jocson <nicpjocson@gmail.com>
2025-01-27 05:38:31 +0800

    Implement initial Search class

--- Search.java
+++ Search.java
@@ -1,2 +1,14 @@
 public class Search {
+    // private int currNumber;
+    // private int min;
+    // private int max;
+
+    // public Search(int min, int max) {
+    //     this.min = min;
+    //     this.max = max;
+    // }
+
+    // public void checkDivisibility() {
+
+    // }
 }
