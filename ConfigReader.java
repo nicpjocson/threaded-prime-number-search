20bf117afd43d820d42fb0e9e7a0ba9f9d6b29f5
Nicole Jocson <nicpjocson@gmail.com>
2025-01-27 05:35:30 +0800

    Implement reading config.txt

--- ConfigReader.java
+++ ConfigReader.java
@@ -1,2 +1,35 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ConfigReader {
+    public Map<String, String> getConfig() {
+        String filePath = "config.txt";
+        Map<String, String> configValues = new HashMap<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(" ");
+                if (parts.length == 2) {
+                    configValues.put(parts[0], parts[1]);
+                }
+            }
+        } catch (IOException | NumberFormatException e) {
+            System.err.println("Error reading config file: " + e.getMessage());
+        }
+
+        // debugging
+        // String x = configValues.get("x");
+        // String y = configValues.get("y");
+        // String printVariation = configValues.get("print-variation");
+        // String taskDivisionScheme = configValues.get("task-division-scheme");
+        
+        // System.out.println("x = " + x);
+        // System.out.println("y = " + y);
+        // System.out.println("print-variation = " + printVariation);
+        // System.out.println("task-division-scheme = " + taskDivisionScheme);
+
+        return configValues;
+    }
 }
--- Main.java
+++ Main.java
@@ -3,5 +3,63 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
+        ConfigReader configReader = new ConfigReader();
+
+        Map<String, String> configValues = new HashMap<>();
+        configValues = configReader.getConfig();
+
+        // get config values
+        String x = configValues.get("x");
+        String y = configValues.get("y");
+        String printVariation = configValues.get("print-variation");
+        String taskDivisionScheme = configValues.get("task-division-scheme");
+
+        // convert x and y from String to integer
+        int noOfThreads = Integer.parseInt(x);
+        int upperLimit = Integer.parseInt(y);
+
+        // debug
+        System.out.println(noOfThreads);
+        System.out.println(upperLimit);
+
+        // create x number of threads
+        for(int i = 0; i < noOfThreads; i++) {
+            System.out.println("created thread");
+            // TODO
+        }
+
+        /* 
+         * 
+         * Printing Variation
+         * 
+         */
+
+        // print immediately
+        if(printVariation == "immediate") {
+            System.out.println(printVariation);
+            // TODO
+
+        // wait until all threads are done then print everything
+        } else if(printVariation == "wait") {
+            System.out.println(printVariation);
+            // TODO
+        }
+
+        /*
+         * 
+         * Task Division Scheme
+         * 
+         */
+
+        // straight division of search range. (ie for 1 - 1000 and 4 threads the division will be 1-250, 251-500, and so forth)
+        if(taskDivisionScheme == "straight") {
+            System.out.println(taskDivisionScheme);
+            // TODO
+
+        // search is linear but the threads are for divisibility testing of individual numbers
+        } else if(taskDivisionScheme == "linear") {
+            System.out.println(taskDivisionScheme);
+            // TODO
+        }
     }
 }
