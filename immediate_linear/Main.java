641e1a9d2890b10c8813e7a70750806b25c90e36
Nicole Jocson <nicpjocson@gmail.com>
2025-02-06 06:16:48 +0800

    checkpoint

--- immediate_linear/Main.java
+++ immediate_linear/Main.java
@@ -1,34 +1,127 @@
+import java.util.*;
+
 // config-related
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+
 import java.util.Map;
 import java.util.HashMap;
+// import java.util.List;
+// import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 // timestamp
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
-public class Main {
+public class Main extends Thread {
     public static void main(String[] args) {
         // get config values
         Map<String, Integer> configValues = new HashMap<>();
         configValues = getConfig();
-        int noOfThreads = configValues.get("x");
+        int numThreads = configValues.get("x"); // NOTE" NOT USED
         int upperLimit = configValues.get("y");
 
-        // create x number of threads
-        Thread[] threads = new Thread[noOfThreads];
+        // create list of threads
+        // List<Thread> threads = new ArrayList<>();
+        // TESTING
+        ExecutorService executor = Executors.newFixedThreadPool(numThreads);
 
-        // wah
+        // // crate hashmap of the numbers to check
+        // // with info if its prime and the max divisor for checking divisibility (sqrt of the number)
+        // Map<Integer, MyNumber> numberMap = new HashMap<>();
 
-        // rules??
+        // // initialize map with numbers, prime status, and max divisor (sqrt of number)
+        // for (int num = 1; num <= upperLimit; num++) {
+        //     numberMap.put(num, new MyNumber(null, 2, (int) Math.sqrt(num)));
+        // }
 
-        // for each number only check until its sqrt
+        // hashmap storing the numbers and if they are prime
+        Map<Integer, Boolean> numberMap = new HashMap<>();
 
-        // once a number is prime do not check anymore
+        // for each number, check if prime
+        for (int i = 1; i <= upperLimit; i++) {
+            // get current number and its status
+            int currNumber = i;
+            Boolean isPrime = numberMap.get(currNumber);
 
-        // 
+            if (currNumber <= 1) {
+                continue;
+            }
+
+            // // DETERMINED if prime/composite
+            // if (isPrime != null) {
+            //     continue;
+            // }
+
+            for (int divisor = 2; divisor <= Math.sqrt(currNumber); divisor++) {
+                int currDivisor = divisor;
+                
+                executor.submit(() -> {
+                    if (checkDivisibility(currNumber, currDivisor)) {
+                        // set number as not prime or composite
+                        synchronized (numberMap) {
+                            numberMap.put(currNumber, false);
+                        }
+                    }
+                });
+            }
+        }
+
+            // // UNDETERMINED if prime/composite
+            // // but DONE checking divisibility for current number
+            // if (num.isPrime == null && num.currDivisor > num.maxDivisor) {
+            //     // then set isPrime to TRUE
+            //     num.isPrime = true;
+            // }
+
+            // // UNDETERMINED if prime/composite
+            // // and NOT DONE checking divisibility for current number
+            // if (num.isPrime == null && num.currDivisor < num.maxDivisor) {
+            //     // use a thread to check for its
+            //     Thread thread = new Thread(() -> {
+            //         checkDivisibility(currNumber, num.currDivisor);
+            //         num.currDivisor++;
+            //     });
+            //     threads.add(thread);
+            //     thread.start();
+
+            //     // if returns true
+            //     // set isPrime = true;
+
+            //     // if returns false
+            //     // check divisibilty of 
+            // }
+
+            // executor.submit(() -> {
+            //     // Check divisibility
+            //     if (currNumber % num.currDivisor == 0) {
+            //         synchronized (lock) {
+            //             // If divisible, mark as composite
+            //             if (num.isPrime == null) {
+            //                 num.isPrime = false;
+            //             }
+            //         }
+            //     }
+            // });
+
+            // executor.submit(() -> {
+            //     while (num.currDivisor <= num.maxDivisor && num.isPrime == null) {
+            //         if (currNumber % num.currDivisor == 0) {
+            //             num.isPrime = false;
+            //             break;
+            //         }
+            //         num.currDivisor++;
+            //     }
+            //     if (num.currDivisor > num.maxDivisor) {
+            //         num.isPrime = true;
+            //     }
+            // });
+
+        // // Shutdown the executor
+        // executor.shutdown();
     }
 
     /*
@@ -87,12 +180,28 @@ public class Main {
      * Search functions
      * 
      */
-    //
+    public static boolean checkDivisibility (int dividend, int divisor) {
+        if (dividend % divisor != 0) {
+            return false;
+        }
+
+        return true;
+    }
     
     /*
      * 
      * Misc
      * 
      */
-    //
+    // static class MyNumber {
+    //     Boolean isPrime; // tracks if number is prime
+    //     int currDivisor;
+    //     int maxDivisor; // tracks the max divisor (square root of the number)
+
+    //     MyNumber(Boolean isPrime, int currDivisor, int maxDivisor) {
+    //         this.isPrime = isPrime;
+    //         this.currDivisor = currDivisor;
+    //         this.maxDivisor = maxDivisor;
+    //     }
+    // }
 }
